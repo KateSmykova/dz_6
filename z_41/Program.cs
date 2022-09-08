593b6f4ee3c394a372a8c3c435be599882875260
Katya <katya0897@yandex.ru>
2022-09-08 23:21:18 +0300

    resh_z41

---
+++ z_41/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write($"Введите количество чисел М (количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arrayNum = new int[m];
+
+void InputNum(int m){
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+    arrayNum[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+int Comparison(int[] arrayNum)
+{
+  int count = 0;
+  for (int i = 0; i < arrayNum.Length; i++)
+  {
+    if(arrayNum [i] > 0 )
+     count += 1; 
+  }
+  return count;
+}
+
+InputNum(m);
+Console.WriteLine($"Количество чисел больше нуля: {Comparison(arrayNum)} ");
